46389b3b9309d060842db3f6df90fc39d70951c0
Simon Chrzanowski <simon@hipaway.com>
2013-06-06 14:47:53 +0200

    update

---
+++ Router/I18nRouter.php
@@ -0,0 +1,10 @@
+<?php
+namespace Hip\I18nRoutingBundle;
+
+use Symfony\Cmf\Component\Routing\ChainedRouterInterface;
+use Symfony\Cmf\Component\Routing\VersatileGeneratorInterface;
+
+class I18nRouter implements ChainedRouterInterface
+{
+
+}
