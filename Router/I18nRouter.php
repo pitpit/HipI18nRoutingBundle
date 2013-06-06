b91726d9c1246eebe16c6679a15a02b4d1d0f251
Simon Chrzanowski <simon@hipaway.com>
2013-06-06 16:07:56 +0200

    updates

--- Router/I18nRouter.php
+++ Router/I18nRouter.php
@@ -6,12 +6,34 @@ use Symfony\Cmf\Component\Routing\VersatileGeneratorInterface;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
 
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\Config\ConfigCache;
+
+
 class I18nRouter implements ChainedRouterInterface
 {
     /**
      * @var RequestContext
      */
     protected $context;
+
+    /**
+     * @var UrlMatcherInterface|null
+     */
+    protected $matcher;
+
+    /**
+     * @var array
+     */
+    protected $options = array();
+
+
+    public function __construct($container, RequestContext $context = null, array $options = array())
+    {
+        $this->setOptions($options);
+        $this->context = null === $context ? new RequestContext() : $context;
+        $this->container = $container;
+    }
 
 
     /**
@@ -51,17 +73,17 @@ class I18nRouter implements ChainedRouterInterface
      */
     public function match($pathinfo)
     {
-
-        return array (
+        $params = array (
             '_controller' => 'Hip\I18nRoutingBundle\Controller\RedirectController::redirectAction',
-            //'path' => $url,
-            //'host' => $host,
+            'path' => $pathinfo,
             'permanent' => true,
             'scheme' => $this->context->getScheme(),
             'httpPort' => $this->context->getHttpPort(),
             'httpsPort' => $this->context->getHttpsPort(),
             '_route' => ''//$params['_route']
         );
+
+        return $params;
     }
 	
 	/**
@@ -78,5 +100,91 @@ class I18nRouter implements ChainedRouterInterface
     public function getRouteDebugMessage($name, array $parameters = array())
     {
         return "Route '$name' not found";
+    }
+
+
+
+
+
+
+
+
+    /**
+     * Gets the UrlMatcher instance associated with this Router.
+     *
+     * @return UrlMatcherInterface A UrlMatcherInterface instance
+     */
+    public function getMatcher()
+    {
+        if (null !== $this->matcher) {
+            return $this->matcher;
+        }
+
+        if (null === $this->options['cache_dir'] || null === $this->options['matcher_cache_class']) {
+            return $this->matcher = new $this->options['matcher_class']($this->getRouteCollection(), $this->context);
+        }
+
+        $class = $this->options['matcher_cache_class'];
+        $cache = new ConfigCache($this->options['cache_dir'].'/'.$class.'.php', $this->options['debug']);
+        if (!$cache->isFresh($class)) {
+            $dumper = new $this->options['matcher_dumper_class']($this->getRouteCollection());
+
+            $options = array(
+                'class' => $class,
+                'base_class' => $this->options['matcher_base_class'],
+            );
+
+            $cache->write($dumper->dump($options), $this->getRouteCollection()->getResources());
+        }
+
+        require_once $cache;
+
+        return $this->matcher = new $class($this->context);
+    }
+
+
+    /**
+     * Sets options.
+     *
+     * Available options:
+     *
+     * * cache_dir: The cache directory (or null to disable caching)
+     * * debug: Whether to enable debugging or not (false by default)
+     * * resource_type: Type hint for the main resource (optional)
+     *
+     * @param array $options An array of options
+     *
+     * @throws \InvalidArgumentException When unsupported option is provided
+     */
+    public function setOptions(array $options)
+    {
+        $this->options = array(
+            'cache_dir' => null,
+            'debug' => false,
+            'generator_class' => 'Symfony\\Component\\Routing\\Generator\\UrlGenerator',
+            'generator_base_class' => 'Symfony\\Component\\Routing\\Generator\\UrlGenerator',
+            'generator_dumper_class' => 'Symfony\\Component\\Routing\\Generator\\Dumper\\PhpGeneratorDumper',
+            'generator_cache_class' => 'ProjectUrlGenerator',
+            'matcher_class' => 'Symfony\\Component\\Routing\\Matcher\\UrlMatcher',
+            'matcher_base_class' => 'Symfony\\Component\\Routing\\Matcher\\UrlMatcher',
+            'matcher_dumper_class' => 'Symfony\\Component\\Routing\\Matcher\\Dumper\\PhpMatcherDumper',
+            'matcher_cache_class' => 'ProjectUrlMatcher',
+            'resource_type' => null,
+            'strict_requirements' => true,
+        );
+
+        // check option names and live merge, if errors are encountered Exception will be thrown
+        $invalid = array();
+        foreach ($options as $key => $value) {
+            if (array_key_exists($key, $this->options)) {
+                $this->options[$key] = $value;
+            } else {
+                $invalid[] = $key;
+            }
+        }
+
+        if ($invalid) {
+            throw new \InvalidArgumentException(sprintf('The Router does not support the following options: "%s".', implode('", "', $invalid)));
+        }
     }
 }
